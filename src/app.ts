24729309617b9deab5da2b5d0c826356e0887d5a
gu.amaralb <gu.amaralb@gmail.com>
2023-05-31 11:44:07 -0300

    CORS problem "solved"

--- src/app.ts
+++ src/app.ts
@@ -10,6 +10,13 @@ const app = express()
 
 app.use(express.json())
 
+app.use((req: Request, res: Response, next: NextFunction) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
+
 app.get('/teste.html', async (req: Request, res: Response) => {
     res.sendFile(path.join(__dirname+'../../teste.html'))
 })
