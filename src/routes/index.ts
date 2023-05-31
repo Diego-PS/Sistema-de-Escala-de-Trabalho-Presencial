b162916c2100f09ab24ceac6d6fb6cf26bba3a98
Diego-PS <diegopsilvavrb@gmail.com>
2023-05-30 22:40:40 -0300

    add login route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import { bossRoutes } from "./bossRoutes";
 import { teamLeaderRoutes } from "./teamLeaderRoutes";
 import { memberRoutes } from "./memberRoutes";
+import { loginRoute } from "./loginRoute";
 
 const router = Router()
 
 router.use(bossRoutes)
 router.use(teamLeaderRoutes)
 router.use(memberRoutes)
+router.use(loginRoute)
 
 export { router }
