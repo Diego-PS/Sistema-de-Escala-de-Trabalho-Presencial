d7a3bcce6c3b27b027c7f8a2d4ee1bbfaa25b1fa
Diego-PS <diegopsilvavrb@gmail.com>
2023-06-15 21:42:33 -0300

    package update

--- src/services/tests/bossServices.test.ts
+++ src/services/tests/bossServices.test.ts
@@ -20,11 +20,34 @@ describe('Testing bossService', () => {
                 mpw: 0
             }
         })
-        expect(boss_to_be_created.name).toBe('TestBoss')
         await bossServices.create(boss_to_be_created)
         const created_boss = await bossServices.getByUsername('TestBoss')
         expect(created_boss.organization_name).toBe('Test Organization')
         await created_boss.delete()
+    })
+
+    test('Update boss', async () => {
+        const boss_to_be_created = new Boss({
+            organization_name: 'Test Organization',
+            name: 'TestBoss',
+            username: 'TestBoss',
+            password: '123456',
+            organization_rules: {
+                moa: 0,
+                mpw: 0
+            }
+        })
+        await bossServices.create(boss_to_be_created)
+        const created_boss = await bossServices.getByUsername('TestBoss')
+        bossServices.update(created_boss.id, { name: 'NewTestBoss'})
+        const updated_boss = await bossServices.getByUsername('TestBoss')
+        expect(updated_boss.name).toBe('NewTestBoss')
+
+        await updated_boss.delete()
         await mongoose.disconnect()
+    })
+
+    test('should pass', () => {
+        expect(1+1).toBe(2)
     })
 })
