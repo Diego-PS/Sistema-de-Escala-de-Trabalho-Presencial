44c9a1f1440428e422c8949a04a815fcee3bd7cd
Diego-PS <diegopsilvavrb@gmail.com>
2023-06-25 20:32:16 -0300

    some integration tests

--- src/services/tests/teamLeader.test.ts
+++ src/services/tests/teamLeader.test.ts
@@ -1,53 +1,50 @@
 import { expect } from '@jest/globals';
-import { teamLeaderServices } from ".."
+import { bossServices, teamLeaderServices } from ".."
 import { TeamLeader } from '../../entities/TeamLeader';
 import { connectDB, disconnectDB } from '../../connectDB';
-import { uptime } from 'process';
+import { Boss } from '../../entities/Boss';
+
+const addFile = (text: string) => text + 'teamLeader'
 
 describe('Testing TeamLeaderService', () => {
 
     test('Creating TeamLeader', async () => {
         await connectDB()
-        const teamLeader_to_be_created = new TeamLeader({
-            boss_id: 'boss_id',
-            name: 'TestTeamLeader',
-            username: 'TestTeamLeader',
+        const boss_to_be_created = new Boss({
+            organization_name: addFile('Organization'),
+            name: addFile('Boss'),
+            username: addFile('Boss'),
             password: '123456',
-            team_name: 'teste',
-            team_rules: {
+            organization_rules: {
                 moa: 0,
                 mpw: 0
             }
         })
-        await teamLeaderServices.create(teamLeader_to_be_created)
-        const created_TeamLeader = await teamLeaderServices.getByUsername('TestTeamLeader')
-        expect(created_TeamLeader.name).toBe('TestTeamLeader')
-        await created_TeamLeader.delete()
-    })
-
-    test('Changing Team Rules', async () => {
-        await connectDB()
-        const teamLeader_to_be_created = new TeamLeader({
-            boss_id: 'boss_id',
-            name: 'TestTeamLeader',
-            username: 'TestTeamLeader',
-            password: '123456',
-            team_name: 'teste',
-            team_rules: {
-                moa: 0,
-                mpw: 0
-            }
-        })
-        await teamLeaderServices.create(teamLeader_to_be_created)
-        const created_TeamLeader = await teamLeaderServices.getByUsername('TestTeamLeader')
+        await bossServices.create(boss_to_be_created)
+        const created_boss = await bossServices.getByUsername(boss_to_be_created.username)
         
-        teamLeaderServices.changeTeamRules(created_TeamLeader.id, { moa: 1 , mpw: 1})
-        const updated_team_leader = await teamLeaderServices.getByUsername('TestTeamLeader')
-        expect(updated_team_leader.team_rules).toBe({moa: '1' , mpw: '1'})
+        const team_leader_to_be_created = new TeamLeader({
+            boss_id: created_boss.id,
+            name: addFile('TestTeamLeader'),
+            username: addFile('TestTeamLeader'),
+            password: '123456',
+            team_name: addFile('TestTeam'),
+            team_rules: {
+                moa: 0,
+                mpw: 0
+            }
+        })
+        await teamLeaderServices.create(team_leader_to_be_created)
+        const created_team_leader = await teamLeaderServices.getById(team_leader_to_be_created.id)
+        expect(created_team_leader.name).toBe(team_leader_to_be_created.name)
+        
+        // created_team_leader.changeTeamRules({ moa: 1, mpw: 30 })
+        // const updated_team_leader = await teamLeaderServices.getById(team_leader_to_be_created.id)
+        // expect(updated_team_leader.team_rules.getMOA()).toBe(1)
+        // expect(updated_team_leader.team_rules.getMPW()).toBe(30)
 
-        await updated_team_leader.delete()
-        await created_TeamLeader.delete()
+        // await updated_team_leader.delete()
+        await created_boss.delete()
         await disconnectDB()
-        
     })
 })
